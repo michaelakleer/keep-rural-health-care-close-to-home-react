7e65874204d03f7b2e5a561671a284bebc29ba7d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-05 19:08:54 -0600

    Updated App.js and styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import News from "./components/News";
-import Archive from "./components/Archive";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <Route exact path="/" component={News} />
-          <Route exact path="/news/:id" component={Archive} />
-        </Fragment>
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
