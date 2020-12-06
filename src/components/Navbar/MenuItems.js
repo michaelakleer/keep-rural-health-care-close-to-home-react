38645a89c2f58dcac1dd7afc670c088e22e1e831
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-05 19:09:32 -0600

    Created new components and styling

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,38 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "/about",
+    cName: "nav-links",
+  },
+  {
+    title: "Research",
+    url: "/research",
+    cName: "nav-links",
+  },
+  {
+    title: "Resources",
+    url: "/resources",
+    cName: "nav-links",
+  },
+  {
+    title: "Community",
+    url: "/community",
+    cName: "nav-links",
+  },
+  {
+    title: "Events",
+    url: "/events",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Contact",
+    url: "/contact",
+    cName: "nav-links-mobile",
+  },
+];
