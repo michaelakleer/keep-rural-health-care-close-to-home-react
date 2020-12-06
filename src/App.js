032d99bd589db0b1686a9fff8d2ee8628ed348c6
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-05 18:04:58 -0600

    Deleted/Changed files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import News from "./components/News";
+import Archive from "./components/Archive";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Route exact path="/" component={News} />
+          <Route exact path="/news/:id" component={Archive} />
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
