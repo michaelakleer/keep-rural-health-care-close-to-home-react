198b91c4831e66bfe882a6974eb1b87ad522bdf3
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-07 15:26:48 -0600

    Imported and routed components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import "react-bulma-components/dist/react-bulma-components.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Research from "./pages/Research/Research";
+import Resources from "./pages/Resources/Resources";
+import Community from "./pages/Community/Community";
+import Events from "./pages/Events/Events";
+import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
@@ -15,7 +20,12 @@ export default function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/research" component={Research} />
+          <Route path="/resources" component={Resources} />
+          <Route path="/community" component={Community} />
+          <Route path="/events" component={Events} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
