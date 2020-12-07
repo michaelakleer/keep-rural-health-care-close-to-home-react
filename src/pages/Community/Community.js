dce0b94c29636f3a269dbe848bea66081cd50c1c
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-07 15:28:55 -0600

    Created Community component and added styling

---
+++ src/pages/Community/Community.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Community.css";
+
+export default function Community() {
+  return (
+    <div className="Community">
+      <section className="communityOne">
+        <h1>Yo</h1>
+      </section>
+      <section className="communityTwo">
+        <h1>Yo</h1>
+      </section>
+      <section className="communityThree">
+        <h1>Yo</h1>
+      </section>
+    </div>
+  );
+}
