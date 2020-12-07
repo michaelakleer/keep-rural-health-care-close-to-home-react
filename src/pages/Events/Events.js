c76894c945943fee338757edbea8e0b223061d9f
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-07 15:29:11 -0600

    Created Events component and added styling

---
+++ src/pages/Events/Events.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Events.css";
+
+export default function Events() {
+  return (
+    <div className="Events">
+      <section className="eventsOne">
+        <h1>Yo</h1>
+      </section>
+      <section className="eventsTwo">
+        <h1>Yo</h1>
+      </section>
+      <section className="eventsThree">
+        <h1>Yo</h1>
+      </section>
+    </div>
+  );
+}
