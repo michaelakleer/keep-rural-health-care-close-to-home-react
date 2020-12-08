8691791c4baba6eda596bd9a1b33c90d51279e8d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-08 14:11:53 -0600

    Included Link for contact button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import { Button } from "../Button";
 import "./Navbar.css";
@@ -30,7 +31,9 @@ class Navbar extends Component {
             );
           })}
         </ul>
-        <Button>Contact</Button>
+        <Link to="/contact">
+          <Button>Contact</Button>{" "}
+        </Link>
       </nav>
     );
   }
