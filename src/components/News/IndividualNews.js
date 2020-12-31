3d6b717de1fd9c9b061f2d7a5b5b3f09f45fb2f2
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-30 18:28:09 -0600

    Created new component and styled

---
+++ src/components/News/IndividualNews.js
@@ -0,0 +1,47 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./IndividualNews.css";
+
+export class IndividualNews extends Component {
+  state = {
+    news: {},
+    isLoaded: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:8000/wp-json/wp/v2/news/${this.props.match.params.id}`
+      )
+      .then((res) =>
+        this.setState({
+          news: res.data,
+          isLoaded: true,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const { news, isLoaded } = this.state;
+    if (isLoaded) {
+      return (
+        <Fragment>
+          <div className="IndividualNews">
+            <Link to="/">Go Back</Link>
+            <hr />
+            <h1>{news.title.rendered}</h1>
+            <div
+              dangerouslySetInnerHTML={{ __html: news.content.rendered }}
+            ></div>
+            <h4>Author: {news.acf.author}</h4>
+          </div>
+        </Fragment>
+      );
+    }
+    return <h3>Loading...</h3>;
+  }
+}
+
+export default IndividualNews;
