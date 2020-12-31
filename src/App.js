bc6df8aa67247d18d58dbbc4ebcf940f5e977b45
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-30 18:29:11 -0600

    Added new routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Resources from "./components/Resources/Resources";
 import Community from "./components/Community/Community";
 import Events from "./components/Events/Events";
 import Contact from "./components/Contact/Contact";
+import Archive from "./components/Archive/Archive";
+import IndividualNews from "./components/News/IndividualNews";
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
@@ -25,6 +27,8 @@ export default function App() {
         <Route path="/community" component={Community} />
         <Route path="/events" component={Events} />
         <Route path="/contact" component={Contact} />
+        <Route path="/archive" component={Archive} />
+        <Route path="/news/:id" component={IndividualNews} />
       </Switch>
       <Footer />
     </div>
