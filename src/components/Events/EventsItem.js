e5810c44be6f0b55fc72e767c7d486a79c147ef7
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-05 17:58:21 -0600

    Updated EventsItem.js

--- src/components/Events/EventsItem.js
+++ src/components/Events/EventsItem.js
@@ -2,3 +2,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import axios from "axios";
+
+export class EventsItem extends Component {
+state = {
+    
+}
+
+}
