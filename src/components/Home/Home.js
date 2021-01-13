f9580908607716c6f9c3db800a298c706d8f9590
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-13 16:10:51 -0600

    Updated Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bulma-components";
+import Events from "./../Events/Events";
 import News from "./../News/News";
 
 import "./Home.css";
@@ -23,26 +24,10 @@ export default function Home() {
           <div>Downloadable pdfs</div>
         </section>
         <section className="homeThree">
-          <div>Events</div>
+          <Events />
         </section>
       </section>
       <section className="homeFour">
-        <div className="newsOne">
-          <h2>Lorem Ipsum</h2>
-          <p>
-            Etiam pellentesque tortor eu erat iaculis, a efficitur odio pretium.
-            Nunc sed justo lacus. Nam a faucibus tortor, quis venenatis ligula.
-            Curabitur fermentum ac dolor vel rhoncus. Pellentesque pretium neque
-            felis, in tempus lectus pellentesque eget. Vivamus a lobortis metus.
-            Donec vehicula lacinia turpis, quis tristique eros condimentum
-            sodales. Fusce aliquam justo vitae nulla tempus, nec tincidunt erat
-            elementum. Nullam at pulvinar turpis. Pellentesque facilisis ligula
-            id arcu accumsan, vitae auctor augue egestas. Fusce vitae ultricies
-            turpis. Nam a sodales urna. Vestibulum placerat ex eu ex euismod, in
-            consequat nulla accumsan. Pellentesque et neque vel est eleifend
-            feugiat eget ac libero.
-          </p>
-        </div>
         <News />
         <Link to="/archive">
           <Button className="button is-large is-fullwidth">Archive</Button>
