fe1a47d297f73a3f69929357f455a95066d69626
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-16 13:06:25 -0600

    Changed images location

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,71 +1,36 @@
 import React from "react";
 import EventsItem from "./EventsItem";
+import Tree from "../../images/tree.jpg";
 import "./Events.css";
 
-export default function Events() {
+export default function Events(props) {
   return (
     <div className="Events">
       <section className="eventsOne">
-        <EventsItem />
-        <h1>Petitions</h1>
-        <div className="petitions">
-          <p>
-            <a href="/">Lorem ipsum dolor sit amet.</a>
-          </p>
-          <p>
-            <a href="/">Lorem ipsum dolor sit amet.</a>
-          </p>
-        </div>
-      </section>
-      <section className="eventsTwo">
-        <h1>Activations</h1>
-        <div className="activations">
-          <p>
-            <a href="/">Lorem ipsum dolor sit amet.</a>
-          </p>
-          <p>
-            <a href="/">Lorem ipsum dolor sit amet.</a>
-          </p>
-        </div>
-      </section>
-      <section className="eventsThree">
-        <h1>Past Events</h1>
-        <div className="containerOne">
-          <div className="left">
-            <h2>Petitions</h2>
-            <div className="leftUrls">
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <h2>Activations</h2>
-            <div className="rightUrls">
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-              <p>
-                <a href="/">Lorem ipsum dolor sit amet.</a>
-              </p>
-            </div>
-          </div>
-        </div>
+        <h2 className="month">February 2021</h2>
+        <hr />
+        <EventsItem
+          day="Tues"
+          date={9}
+          fullDate="February 9 @ 8:00 am - 5:00 pm"
+          title="Title (link to individual page)"
+          location="Location (Link to google maps page??"
+          description="Description (excerpt)"
+          photoTitle="Tree"
+          imageSource={Tree}
+        />
+        <h2 className="month">March 2021</h2>
+        <hr />
+        <EventsItem
+          day="Wed"
+          date={10}
+          fullDate="March 10 @ 8:00 am - 5:00 pm"
+          title="Title (link to individual page)"
+          location="Location (Link to google maps page??"
+          description="Description (excerpt)"
+          photoTitle="Tree"
+          imageSource={Tree}
+        />
       </section>
     </div>
   );
