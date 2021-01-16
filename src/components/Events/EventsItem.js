851d59e115a472c8b1bb25bee9a797484d95db6b
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-16 13:06:40 -0600

    Added props

--- src/components/Events/EventsItem.js
+++ src/components/Events/EventsItem.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./EventsItem.css";
-import Tree from "./../images/tree.jpg";
 
-export default function EventsItem() {
+export default function EventsItem(props) {
   return (
     <div className="EventsItem">
       <div className="eventContainer">
         <div className="date">
-          <div>Tues</div>
-          <div className="dateNumber">9</div>{" "}
+          <div>{props.day}</div>
+          <div className="dateNumber">{props.date}</div>{" "}
         </div>
         <div className="info">
-          <div className="fullDate">February 9 @ 8:00 am - 5:00 pm</div>
-          <div className="eventTitle">Title (link to individual page)</div>
-          <div className="location">Location (Link to google maps page??)</div>
-          <div className="description">Description (excerpt)</div>
+          <div className="fullDate">{props.fullDate}</div>
+          <div className="eventTitle">{props.title}</div>
+          <div className="location">{props.location}</div>
+          <div className="description">{props.description}</div>
         </div>
         <div className="image">
-          <img src={Tree} alt="Tree" className="treeImage" />
+          <img
+            src={props.imageSource}
+            alt={props.photoTitle}
+            className="treeImage"
+          />
         </div>
       </div>
     </div>
