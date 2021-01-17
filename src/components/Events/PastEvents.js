c4496a0639ab3ebd59bc37c74f0135822e44207d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-17 17:52:06 -0600

    Created new components and styling

---
+++ src/components/Events/PastEvents.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PastEvents.css";
+
+export default function PastEvents() {
+  return (
+    <div className="PastEvents">
+      <section>
+        <h1>Hello</h1>
+      </section>
+    </div>
+  );
+}
