8ae78e3ae67c69aad14dcf63f405094c124ea497
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-17 17:52:24 -0600

    Updated components

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EventsItem from "./EventsItem";
-import Tree from "../../images/tree.jpg";
+import { Link } from "react-router-dom";
+import { Button } from "react-bulma-components";
 import "./Events.css";
 
 export default function Events(props) {
@@ -9,54 +10,17 @@ export default function Events(props) {
       <section className="eventsOne">
         <h2 className="month">February 2021</h2>
         <hr />
-        <EventsItem
-          id="feb-9"
-          day="Tue"
-          date={9}
-          fullDate="February 9 @ 8:00 am - 5:00 pm"
-          title="Title (link to individual page)"
-          location="Location (Link to google maps page??"
-          description="Description (excerpt)"
-          photoTitle="Tree"
-          imageSource={Tree}
-        />
+        <EventsItem />
         <h2 className="month">March 2021</h2>
         <hr />
-        <EventsItem
-          id="mar-10"
-          day="Wed"
-          date={10}
-          fullDate="March 10 @ 8:00 am - 5:00 pm"
-          title="Title (link to individual page)"
-          location="Location (Link to google maps page??"
-          description="Description (excerpt)"
-          photoTitle="Tree"
-          imageSource={Tree}
-        />
-        <EventsItem
-          id="mar-12"
-          day="Fri"
-          date={12}
-          fullDate="March 12 @ 8:00 am - 5:00 pm"
-          title="Title (link to individual page)"
-          location="Location (Link to google maps page??"
-          description="Description (excerpt)"
-          photoTitle="Tree"
-          imageSource={Tree}
-        />
+        <EventsItem />
+        <EventsItem />
         <h2 className="month">April 2021</h2>
         <hr />
-        <EventsItem
-          id="apr-9"
-          day="Fri"
-          date={9}
-          fullDate="April 9 @ 8:00 am - 5:00 pm"
-          title="Title (link to individual page)"
-          location="Location (Link to google maps page??"
-          description="Description (excerpt)"
-          photoTitle="Tree"
-          imageSource={Tree}
-        />
+        <EventsItem />
+        <Link to="/events/archive">
+          <Button className="button is-large">Past Events</Button>
+        </Link>
       </section>
     </div>
   );
--- src/components/Events/EventsItem.js
+++ src/components/Events/EventsItem.js
@@ -1,31 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { data } from "./EventData";
 import "./EventsItem.css";
 
-export default function EventsItem(props) {
+export const EventsItem = () => {
   return (
-    <div className="EventsItem">
+    <>
       <div className="eventContainer">
+        {data.map((data, key) => {
+          return (
+            <div key={key}>
+              <Event
+                key={key}
+                id={data.id}
+                day={data.day}
+                date={data.date}
+                fullDate={data.fullDate}
+                title={data.title}
+                location={data.location}
+                description={data.description}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+      };
+
+  const Event = ({ id, day, date, fullDate, title, location, description }) => {
+    if (!id) return <div />;
+    return (
+      <div className="EventItem">
         <div className="date">
-          <div>{props.day}</div>
-          <div className="dateNumber">{props.date}</div>{" "}
+          <div>{data.day}</div>
+          <div className="dateNumber">{data.date}</div>{" "}
         </div>
         <div className="info">
-          <div className="fullDate">{props.fullDate}</div>
+          <div className="fullDate">{data.fullDate}</div>
           <div className="eventTitle">
-            <Link to={`/events/${props.id}`}>{props.title}</Link>
+            <Link to={`/events/${data.id}`}>{data.title}</Link>
           </div>
-          <div className="location">{props.location}</div>
-          <div className="description">{props.description}</div>
+          <div className="location">{data.location}</div>
+          <div className="description">{data.description}</div>
         </div>
         <div className="image">
-          <img
-            src={props.imageSource}
-            alt={props.photoTitle}
-            className="image"
-          />
+          <img src={data.imageSource} alt={data.photoTitle} className="image" />
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+};
+
+export default EventsItem;
