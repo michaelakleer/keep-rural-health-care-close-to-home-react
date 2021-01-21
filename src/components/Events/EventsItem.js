3ae042c4f894bb842dcda3070d74b23eade180ad
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-21 17:08:27 -0600

    Updated EventsItem.js with previous data

--- src/components/Events/EventsItem.js
+++ src/components/Events/EventsItem.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./EventsItem.css";
 
-export default function EventsItem() {
-  return <div></div>;
+export default function EventsItem(props) {
+  return (
+    <div className="EventsItem">
+      <div className="eventContainer">
+        <div className="date">
+          <div>{props.day}</div>
+          <div className="dateNumber">{props.date}</div>{" "}
+        </div>
+        <div className="info">
+          <div className="fullDate">{props.fullDate}</div>
+          <div className="eventTitle">{props.title}</div>
+          <div className="location">{props.location}</div>
+          <div className="description">{props.description}</div>
+        </div>
+        <div className="image">
+          <img
+            src={props.imageSource}
+            alt={props.photoTitle}
+            className="treeImage"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
