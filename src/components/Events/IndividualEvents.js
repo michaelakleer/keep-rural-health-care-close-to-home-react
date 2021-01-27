f909091af7530e9d5c780cb820a419173cceb133
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-27 17:41:06 -0600

    Updated data

---
+++ src/components/Events/IndividualEvents.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function IndividualEvents() {
+  return <div>Hello</div>;
+}
--- src/components/Events/PastEvents.js
+++ src/components/Events/PastEvents.js
@@ -5,6 +5,8 @@ export default function PastEvents() {
   return (
     <div className="PastEvents">
       <section>
+        <hr />
+        <h1>2020</h1>
         <h1>Hello</h1>
       </section>
     </div>
